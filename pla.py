92136f2ec1f65bb551b35454525a427509827072
amanbangeraa <amanbangera2023@gmail.com>
2026-06-15 09:55:40 +0530

    Add pla.py

---
+++ pla.py
@@ -0,0 +1,16 @@
+class node:
+    def __init__(self, data):
+        self.left = None
+        self.data = data
+        self.right = None
+        
+def inorder(root):
+    if root
+        
+a =  node(1)
+b = node(2)
+c = node(3)
+a.left = b
+a.right = c
+print(a.data, a.left.data, a.right.data)
+        
